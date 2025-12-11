b637fc1b8625564e54a9a553affe2043797b238a
kanoshiou <brycegjh@outlook.com>
2025-12-11 13:17:33 +0800

    current program data may be returned from previous day

--- api/client.go
+++ api/client.go
@@ -81,14 +81,45 @@ func GetStreamURLs(stationID string) ([]string, error) {
 // ProgramURLFmt is the program info API URL format
 const ProgramURLFmt = "https://api.radiko.jp/program/v4/date/%s/station/%s.json"
 
+var jst *time.Location
+
+func init() {
+	// Use Japan timezone (UTC+9)
+	jst = time.FixedZone("JST", 9*60*60)
+}
+
 // GetCurrentProgram retrieves the current program for a station
 func GetCurrentProgram(stationID string) (*model.Program, error) {
-	// Use Japan timezone (UTC+9)
-	jst := time.FixedZone("JST", 9*60*60)
 	now := time.Now().In(jst)
 	dateStr := now.Format("20060102")
 	timeStr := now.Format("20060102150405")
 
+	// Try to get program for current date
+	prog, err := getProgramForDate(stationID, dateStr, timeStr)
+	if err != nil {
+		return nil, err
+	}
+
+	// If program found, return it
+	if prog != nil {
+		return prog, nil
+	}
+
+	// If no program found, the API might have returned next day's data
+	// Try fetching previous day's data
+	yesterday := now.AddDate(0, 0, -1)
+	yesterdayStr := yesterday.Format("20060102")
+
+	prog, err = getProgramForDate(stationID, yesterdayStr, timeStr)
+	if err != nil {
+		return nil, err
+	}
+
+	return prog, nil
+}
+
+// getProgramForDate retrieves program data for a specific date and finds the current program
+func getProgramForDate(stationID, dateStr, timeStr string) (*model.Program, error) {
 	url := fmt.Sprintf(ProgramURLFmt, dateStr, stationID)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -113,6 +144,17 @@ func GetCurrentProgram(stationID string) (*model.Program, error) {
 	// Find the program for the current time
 	for _, station := range progResp.Stations {
 		if station.StationID == stationID {
+			// Check if the first program starts after current time
+			// This indicates the current program data is in the previous day's API data
+			if len(station.Programs.Program) > 0 {
+				firstProgram := station.Programs.Program[0]
+				if firstProgram.Ft > timeStr {
+					// First program is in the future, current program data is from previous day
+					return nil, nil
+				}
+			}
+
+			// Find the program that matches current time
 			for _, prog := range station.Programs.Program {
 				// Check if current time is within the program's time range
 				if prog.Ft <= timeStr && timeStr < prog.To {
